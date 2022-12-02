d4613652f638d2a01b5881d34a915d840da9e426
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-02 12:52:59 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,25 @@
+export const item = {
+    "id": "c58b1d7f-30cb-48bf-b947-622fbf843f30",
+    "home_feed": [
+        {
+            "id": "738e0cf2-568c-49b8-a6f3-98426f3cf0ce",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/231.png"
+        },
+        {
+            "id": "71c9d430-faab-484a-8474-f3b102e46bf0",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/14.png"
+        },
+        {
+            "id": "cf70d891-4886-400d-a7da-2f88ea5698dd",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+        },
+        {
+            "id": "193fb7da-2474-4306-9789-df60c6390284",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+        },
+        {
+            "id": "474de028-701f-4754-95ca-03730122ed88",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/143.png"
+        }
+    ]
+}
